f98f1e76bc10eaf111d4290dbc840cac7c102e6e
Felix Henninger <mailbox@felixhenninger.com>
2016-10-19 12:41:23 +0200

    Add example to documentation

--- R/api.r
+++ R/api.r
@@ -155,6 +155,29 @@ PCS_run <- function(interconnection_matrix, initial_state, resting_levels, reset
 #'
 #' @inheritParams PCS_run
 #'
+#' @examples
+#'
+#' # Build interconnection matrix
+#' interconnections <- matrix(
+#'   c( 0.0000,  0.1015,  0.0470,  0.0126,  0.0034,  0.0000,  0.0000,
+#'      0.1015,  0.0000,  0.0000,  0.0000,  0.0000,  0.0100, -0.0100,
+#'      0.0470,  0.0000,  0.0000,  0.0000,  0.0000,  0.0100, -0.0100,
+#'      0.0126,  0.0000,  0.0000,  0.0000,  0.0000,  0.0100, -0.0100,
+#'      0.0034,  0.0000,  0.0000,  0.0000,  0.0000, -0.0100,  0.0100,
+#'      0.0000,  0.0100,  0.0100,  0.0100, -0.0100,  0.0000, -0.2000,
+#'      0.0000, -0.0100, -0.0100, -0.0100,  0.0100, -0.2000,  0.0000 ),
+#'   nrow=7
+#'   )
+#'
+#' # Run model
+#' result <- PCS_run_from_interconnections(interconnections)
+#'
+#' # Examine iterations required for convergence
+#' result$convergence
+#'
+#' # Examine final model state
+#' result$iterations[nrow(result$iterations),]
+#'
 #' @export
 PCS_run_from_interconnections <- function(interconnection_matrix,
   convergence_criteria=c(PCS_convergence_McCandR), convergence_names="default") {
